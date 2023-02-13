2666abbe3ce57f20f88d80269b46cebf9bb16523
praditya-alpha-lion <praditya@alphalionlogistics.com>
2023-02-14 03:56:39 +0530

    login credentials by default added

--- src/screens/authentication/Login.jsx
+++ src/screens/authentication/Login.jsx
@@ -95,6 +95,7 @@ export default function Login() {
                           placeholder='Username'
                           {...register("email")}
                           required
+                          value="praditya@alphalionlogistics.com"
                         />
                       </div>
                       <div className='mb-4'>
@@ -102,6 +103,7 @@ export default function Login() {
                           type='password'
                           className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                           placeholder='Password'
+                          value={"12345"}
                           {...register("password")}
                           required
                         />
